5f6a502edddffbcf0d1ae5753feb39e5e9b4a96b
Robbie Groenewoudt <robbie.groenewoudt@gmail.com>
2024-07-02 20:03:56 +0200

    Support TimeOnly

---
+++ NetBike.Xml/Converters/Basics/XmlTimeOnlyConverter.cs
@@ -0,0 +1,22 @@
+﻿#if NET8_0_OR_GREATER
+
+namespace NetBike.Xml.Converters.Basics
+{
+    using System;
+    using System.Xml;
+
+    public sealed class XmlTimeOnlyConverter : XmlBasicRawConverter<TimeOnly>
+    {
+        protected override TimeOnly Parse(string value, XmlSerializationContext context)
+        {
+            return TimeOnly.FromTimeSpan(XmlConvert.ToTimeSpan(value));
+        }
+
+        protected override string ToString(TimeOnly value, XmlSerializationContext context)
+        {
+            return XmlConvert.ToString(value.ToTimeSpan());
+        }
+    }
+}
+
+#endif
